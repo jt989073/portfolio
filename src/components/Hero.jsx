b604b07d079b2c26790798a94ee2cdcc09ccb856
jt989073 <thompsonjames0120@gmail.com>
2024-08-28 00:05:43 -0500

    added scrolller to hero and adjusted for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,12 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+
 const Hero = () => {
   return (
-    <section className="relative w-full h-[1000px] mx-auto">
+    <section className="relative w-full h-screen mx-auto">
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[90px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
@@ -19,17 +20,32 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a software engineer specializing in developing
-            innovative web applications with a multitude of full-stack technologies.
-            <br className="sm:block hidden"/>
-            <br className="sm:block hidden"/>
-            Currently, I teach at App Academy, empowering students to master
-            software development, from zero to hero. My passion lies in leveraging technology to
-            create impactful solutions.
+            <br className="sm:block hidden" />
+            innovative web applications with a multitude of full-stack
+            technologies.
           </p>
         </div>
       </div>
-      
-        <ComputersCanvas />
+
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { meshBounds, OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("/desktop_pc/scene.gltf");
   return (
     <mesh>
@@ -19,19 +19,37 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.55}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.15]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, 0]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState();
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop="demand"
       shadows
+      dpr={[1,2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
@@ -41,7 +59,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
