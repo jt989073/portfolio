d2784a449336e97489535baa4d2ffac6d760c042
jt989073 <thompsonjames0120@gmail.com>
2024-08-28 23:04:33 -0500

    added placeholder info for projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -16,7 +16,40 @@ const ProjectCard = ({
   image,
   source_code_link,
 }) => {
-  return <motion.div>test</motion.div>;
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link)}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img src={github} alt={github} className="w-1/2 h-1/2 object-contain" />
+              {/* #TODO: add live link wheen ready */}
+              {/* <img src={github} alt={github} className="w-1/2 h-1/2 object-contain" /> */}
+            </div>
+          </div>
+        </div>
+        <div className="mt-5 ">
+          <h3>{name}</h3>
+          <p>{description}</p>
+        </div>
+      </Tilt>
+    </motion.div>
+  );
 };
 
 const Works = () => {
